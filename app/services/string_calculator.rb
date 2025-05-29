e7b7071a6017e83fae28a1eafa2564c22bfd9d47
AparnaVisw <viswanath.aparna@gmail.com>
2025-05-29 10:14:50 +0530

    Refactored code

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,18 +2,12 @@ class StringCalculator
   def self.add(input)
     return 0 if input.blank?
 
-    delimiter, numbers_string = parse_input(input)
-    validate_format!(numbers_string, delimiter)
+    delimiter_regex, numbers_string = parse_input(input)
+    validate_format!(numbers_string, delimiter_regex)
 
-    numbers = numbers_string
-              .split(Regexp.new(delimiter))
-              .reject(&:blank?)
-              .map(&:to_i)
+    numbers = extract_numbers(numbers_string, delimiter_regex)
 
-    negatives = numbers.select(&:negative?)
-    if negatives.any?
-      raise ArgumentError, "Negatives not allowed: #{negatives.join(', ')}"
-    end
+    check_for_negatives!(numbers)
 
     numbers.reject { |n| n > 1000 }.sum
   end
@@ -21,44 +15,58 @@ class StringCalculator
   private
 
   def self.parse_input(input)
-    if input.start_with?("//")
-      # Handle multiple delimiters: //[delim1][delim2]...\n
-      parts = input.split("\n", 2)
-      if parts.size < 2 || parts[1].strip.empty?
-        raise ArgumentError, "Custom delimiter format must be //<delimiter>\\n<numbers>"
-      end
+    return [default_delimiter, input] unless input.start_with?("//")
 
-      delimiter_section = parts[0][2..]
+    delimiter_line, numbers_string = input.split("\n", 2)
 
-      # Multiple delimiters e.g. //[***][%]\n1***2%3
-      if delimiter_section.start_with?("[") && delimiter_section.end_with?("]")
-        delimiters = delimiter_section.scan(/\[(.*?)\]/).flatten
-        escaped_delims = delimiters.map { |d| Regexp.escape(d) }
-        delimiter_regex = escaped_delims.join("|")
-      else
-        delimiter_regex = Regexp.escape(delimiter_section)
-      end
+    raise ArgumentError, "Custom delimiter format must be //<delimiter>\\n<numbers>" if numbers_string.blank?
 
-      [delimiter_regex, parts[1]]
+    delimiter_section = delimiter_line[2..]
+    delimiter_regex = build_delimiter_regex(delimiter_section)
+
+    [delimiter_regex, numbers_string]
+  end
+
+  def self.build_delimiter_regex(delimiter_section)
+    if delimiter_section.start_with?("[") && delimiter_section.end_with?("]")
+      delimiters = delimiter_section.scan(/\[(.*?)\]/).flatten
+      delimiters.map! { |d| Regexp.escape(d) }
+      delimiters.join("|")
     else
-      [",|\n", input]
+      Regexp.escape(delimiter_section)
     end
   end
 
-  def self.validate_format!(numbers_string, delimiter)
-    regex = Regexp.new(delimiter)
-  
+  def self.validate_format!(numbers_string, delimiter_regex)
+    regex = Regexp.new(delimiter_regex)
+
     if numbers_string.match?(/\A#{regex}/) || numbers_string.match?(/#{regex}\z/)
       raise ArgumentError, "Delimiter should not appear at the start or end"
     end
-  
+
     if numbers_string.match?(/#{regex}{2,}/)
       raise ArgumentError, "Consecutive delimiters are not allowed"
     end
-  
-    # Allow digits, delimiters, newlines, whitespace, and minus sign for negatives
-    unless numbers_string.match?(/\A[\d\-#{Regexp.escape(delimiter)}\n\s]*\z/)
+
+    unless numbers_string.match?(/\A[\d\-\s#{delimiter_regex}]*\z/)
       raise ArgumentError, "Input contains invalid characters"
     end
+  end
+
+  def self.extract_numbers(string, delimiter_regex)
+    string.split(Regexp.new(delimiter_regex))
+          .reject(&:blank?)
+          .map(&:to_i)
+  end
+
+  def self.check_for_negatives!(numbers)
+    negatives = numbers.select(&:negative?)
+    return if negatives.empty?
+
+    raise ArgumentError, "Negatives not allowed: #{negatives.join(', ')}"
+  end
+
+  def self.default_delimiter
+    ",|\n"
   end
 end
